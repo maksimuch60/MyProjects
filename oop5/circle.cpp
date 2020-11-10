67d977f6bd1c70a468c06dc2523f220f53730ceb
maksimuch60 <73530736+maksimuch60@users.noreply.github.com>
2020-11-10 09:59:04 +0300

    Delete circle.cpp

--- oop5/circle.cpp
+++
@@ -1,9 +0,0 @@
-#include "Circle.h"
-Circle::Circle(const Circle& circle):TwoDShape(circle)
-{
-	size = circle.size;
-	CircLength = circle.CircLength;
-}
-Circle::Circle(int r) : TwoDShape(r) { size = r; CircLength = 2 * 3.1415 * r; }
-void Circle::Print() { std::cout << "Type of shape: Circle\n" << "Area: " << Area() << "\nLength: "<< CircLength <<"\n"; }
-double Circle::Area() { return 3.1415*size * size; }
